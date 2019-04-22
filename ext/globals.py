b3a7a5dab9f6fcd4ec0f606aee3272ac63ca87f7
Feven Kitsune <fevenkitsune@gmail.com>
2019-04-21 21:22:23 -0400

    + isolated more globals

--- ext/globals.py
+++ ext/globals.py
@@ -4,6 +4,8 @@ Author: Feven Kitsune <fevenkitsune@gmail.com>
 
 """
 
+import os
+
 # Globally accessible list of extensions.
 extensions = [
     "ext.coreutilities",
@@ -17,3 +19,7 @@ extensions = [
 COL_MESSAGE = 0xFFB600
 COL_ERROR = 0xFF0000
 DEV_ID = 276531286443556865
+BOT_KEY = os.environ['FU_KEY']
+
+# Config Variables
+BOT_PREFIX = "f."
