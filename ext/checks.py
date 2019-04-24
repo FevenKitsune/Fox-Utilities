3d4ee518fe0ff490c045fb16c86d5e711988ed68
Feven Kitsune <fevenkitsune@gmail.com>
2019-04-24 13:33:35 -0400

    fixes

--- ext/checks.py
+++ ext/checks.py
@@ -8,7 +8,7 @@ import discord
 from discord.ext import commands
 
 
-def is_admin(ctx):
+def is_admin():
     async def predicate(ctx):
         return (
             ctx.message.channel.permissions_for(ctx.message.author).administrator
