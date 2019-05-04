48fa9db68ae6b9b7ac4b623fa9dc4fbab8afb88d
Feven Kitsune <fevenkitsune@gmail.com>
2019-05-03 20:42:53 -0400

    + another test

--- ext/checks.py
+++ ext/checks.py
@@ -8,13 +8,18 @@ import discord
 from discord.ext import commands
 from ext.globals import *
 
+def has_tag(ctx):
+    tag = discord.utils.get(ctx.author.roles, name=str(role_tag[ctx.command.name]))
+    if tag is None:
+        return False
+    retunr True
 
 def is_admin():
     async def predicate(ctx):
         return (
             ctx.message.channel.permissions_for(ctx.message.author).administrator
             or (ctx.author.id == DEV_ID)  # Permissions for dev.
-            or discord.utils.get(ctx.author.roles, name=str(role_tag[ctx.command.name]))
+            or has_tag(ctx)
         )
     return commands.check(predicate)
 
