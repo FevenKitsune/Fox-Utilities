b7b56e5cc639228ae7138597446c0a4979aec553
Feven Kitsune <fevenkitsune@gmail.com>
2019-11-16 12:10:51 -0500

    + organized globals

--- ext/globals.py
+++ ext/globals.py
@@ -24,16 +24,16 @@ message_color = 0xFFB600  # Color of the message embeds.
 error_color = 0xFF0000  # Color of errors
 developer_id = 276531286443556865  # ID of the developer (FevenKitsune)
 bot_key = os.environ['FU_KEY']  # Environment variable name
-# Link to invite the bot
+
+# Bot Links
 bot_invite = "https://discordapp.com/api/oauth2/authorize?client_id=476166340328161280&permissions=8&scope=bot"
-# Link to the bot Development Server
 bot_development_server = "https://discord.gg/ZVJasmz"
-bot_source = "https://github.com/FevenKitsune/Fox-Utilities"  # Link to the bot Github
-bot_description = "Fox Utilities"  # Discord.Bot description input
+bot_source = "https://github.com/FevenKitsune/Fox-Utilities"
 
 # Config Variables
 bot_prefix = "f."  # Default prefix
 bot_default_status = "with code."  # Default status
+bot_description = "Fox Utilities"  # Discord.Bot description input
 
 # Snipe Dict
 snipe_db = {}  # Active storage for snipes
