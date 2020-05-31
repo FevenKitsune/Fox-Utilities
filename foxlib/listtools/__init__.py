3bfb3f89ab23fb557263c377c5a58e8360524f08
Feven Kitsune <fevenkitsune@gmail.com>
2020-05-30 21:11:33 -0400

    + corrected header + added type hinting

--- foxlib/listtools/__init__.py
+++ foxlib/listtools/__init__.py
@@ -1,5 +1,5 @@
 """
-pkg_foxlib > ListTools > __init__.py
+foxlib > ListTools > __init__.py
 Author: Feven Kitsune <fevenkitsune@gmail.com>
 This work is licensed under a This work is licensed under a Creative Commons Attribution 4.0 International License.
 """
--- foxlib/listtools/chunklist.py
+++ foxlib/listtools/chunklist.py
@@ -1,11 +1,11 @@
 """
-pkg_foxlib > ListTools > chunklist.py
+foxlib > ListTools > chunklist.py
 Author: Feven Kitsune <fevenkitsune@gmail.com>
 This work is licensed under a This work is licensed under a Creative Commons Attribution 4.0 International License.
 """
 
 
-def chunklist(inlist: list, chunksize: int):
+def chunklist(inlist: list, chunksize: int) -> list:
     """Split a list into chucks of determined size.
 
     Keyword arguments:
@@ -13,7 +13,7 @@ def chunklist(inlist: list, chunksize: int):
     chunkSize -- number of elements in each chunk
     """
 
-    def __chunkyield():
+    def __chunkyield() -> list:
         # https://www.geeksforgeeks.org/break-list-chunks-size-n-python/
         for i in range(0, len(inlist), chunksize):
             yield inlist[i:i + chunksize]
