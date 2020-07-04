2c020acb7a02de9d79f87fbe23db36a88296cd04
Feven Kitsune <fevenkitsune@gmail.com>
2020-07-04 13:11:28 -0400

    + removed circular import

--- db/engine.py
+++ db/engine.py
@@ -1,6 +1,4 @@
 from sqlalchemy import create_engine
-from main import logger
 
 # Create database in root folder outside of Git repository.
-logger.info("Loading SQLAlchemy engine.")
 engine = create_engine('sqlite:///../../db/db.db')
