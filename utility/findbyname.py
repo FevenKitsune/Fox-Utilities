93eeef1707d6b9acec0e9f2bc4cd4ba8c6fc0f02
Feven Kitsune <fevenkitsune@gmail.com>
2020-07-17 15:58:24 -0400

    + wrong argument name

--- utility/findbyname.py
+++ utility/findbyname.py
@@ -13,7 +13,7 @@ async def find_by_name(name, search_in):
     """Performs a fuzzy search with unicode-font conversions."""
     # found_name = process.extractOne(normalize("NFKC", name), [normalize("NFKC", item.name) for item in search_in])
     # TODO SET THIS BACK TO NORMAL
-    found_name = extract(normalize("NFKC", name), [normalize("NFKC", item.name) for item in search_in], choices=3)
+    found_name = extract(normalize("NFKC", name), [normalize("NFKC", item.name) for item in search_in], limit=3)
     # found_item = find(lambda m: normalize("NFKC", m.name) == found_name[0], search_in)
     found_item = [find(lambda m: normalize("NFKC", m.name) == found, search_in) for found in found_name]
     return found_item
