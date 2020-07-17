59fa4e00c1aa2526de4b301e149066e0b38118e6
Feven Kitsune <fevenkitsune@gmail.com>
2020-07-17 15:43:16 -0400

    + disable cutoff to ensure match

--- utility/findbyname.py
+++ utility/findbyname.py
@@ -13,7 +13,7 @@ async def find_by_name(name, search_in):
     """Performs a fuzzy search with unicode-font conversions."""
     # found_name = process.extractOne(normalize("NFKC", name), [normalize("NFKC", item.name) for item in search_in])
     # TODO SET THIS BACK TO NORMAL
-    found_name = get_close_matches(normalize("NFKC", name), [normalize("NFKC", item.name) for item in search_in], n=3)
+    found_name = get_close_matches(normalize("NFKC", name), [normalize("NFKC", item.name) for item in search_in], n=3, cutoff=0.0)
     # found_item = find(lambda m: normalize("NFKC", m.name) == found_name[0], search_in)
     found_item = [find(lambda m: normalize("NFKC", m.name) == found, search_in) for found in found_name]
     return found_item
