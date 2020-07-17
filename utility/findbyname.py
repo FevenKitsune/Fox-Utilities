36e11940b7048d69a4dd860f6b9d62368006224a
Feven Kitsune <fevenkitsune@gmail.com>
2020-07-17 17:21:38 -0400

    + added fuzzywuzzy conflict resolution

--- utility/findbyname.py
+++ utility/findbyname.py
@@ -9,18 +9,37 @@ from fuzzywuzzy.process import extract
 from unicodedata import normalize
 
 
+def extract_conflicting_scores(extract_list):
+    """Isolates objects in an array with matching fuzzywuzzy scores"""
+    list_builder = []
+    for i in extract_list:
+        if i[1] == extract_list[0][1]:
+            list_builder.append(i)
+
+    return list_builder
+
+
+def build_conflicting_scores_string(conflicting_scores):
+    """Builds a printable string from a conflicting scores list"""
+    string_builder = ""
+    for i in conflicting_scores:
+        string_builder += f"\n\"{i[0].name}\" match score: {i[1]}"
+
+    return string_builder
+
+
 async def find_by_name(name, search_in):
     """Performs a fuzzy search with unicode-font conversions."""
-    # found_name = process.extractOne(normalize("NFKC", name), [normalize("NFKC", item.name) for item in search_in])
     found_name = extract(normalize("NFKC", name), [normalize("NFKC", item.name) for item in search_in])
-    # found_item = find(lambda m: normalize("NFKC", m.name) == found_name[0], search_in)
     found_items = [[find(lambda m: normalize("NFKC", m.name) == found[0], search_in), found[1]] for found in found_name]
 
-    # Check for conflicts
+    # Check for match conflicts
     if (
             len(found_items) > 1
             and found_items[0][1] == found_items[1][1]
     ):
-        raise UserWarning("Conflict!")
+        # Create a warning string that tells the user what conflicts there are.
+        raise UserWarning(f"Conflicting lookup found! Please provide more detail in your search.\n"
+                          f"Conflicts:{build_conflicting_scores_string(extract_conflicting_scores(found_items))}")
 
     return found_items
