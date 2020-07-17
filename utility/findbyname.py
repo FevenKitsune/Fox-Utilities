ca57b44b3a682762bac89613d1f7b312d291b1fc
Feven Kitsune <fevenkitsune@gmail.com>
2020-07-17 15:51:20 -0400

    + switching back to fuzzywuzzy

--- utility/findbyname.py
+++ utility/findbyname.py
@@ -5,7 +5,7 @@ This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 Intern
 """
 
 from discord.utils import find
-from difflib import get_close_matches
+from fuzzywuzzy.process import extract
 from unicodedata import normalize
 
 
@@ -13,7 +13,7 @@ async def find_by_name(name, search_in):
     """Performs a fuzzy search with unicode-font conversions."""
     # found_name = process.extractOne(normalize("NFKC", name), [normalize("NFKC", item.name) for item in search_in])
     # TODO SET THIS BACK TO NORMAL
-    found_name = get_close_matches(normalize("NFKC", name), [normalize("NFKC", item.name) for item in search_in], n=3, cutoff=0.0)
+    found_name = extract(normalize("NFKC", name), [normalize("NFKC", item.name) for item in search_in], choices=3)
     # found_item = find(lambda m: normalize("NFKC", m.name) == found_name[0], search_in)
     found_item = [find(lambda m: normalize("NFKC", m.name) == found, search_in) for found in found_name]
     return found_item
