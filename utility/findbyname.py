98aa375be8164eed71ebf46e19ea464c3286d605
Feven Kitsune <fevenkitsune@gmail.com>
2020-07-17 15:41:31 -0400

    + beta testing new algorithm

--- utility/findbyname.py
+++ utility/findbyname.py
@@ -5,12 +5,15 @@ This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 Intern
 """
 
 from discord.utils import find
-from fuzzywuzzy import process
+from difflib import get_close_matches
 from unicodedata import normalize
 
 
 async def find_by_name(name, search_in):
     """Performs a fuzzy search with unicode-font conversions."""
-    found_name = process.extractOne(normalize("NFKC", name), [normalize("NFKC", item.name) for item in search_in])
-    found_item = find(lambda m: normalize("NFKC", m.name) == found_name[0], search_in)
+    # found_name = process.extractOne(normalize("NFKC", name), [normalize("NFKC", item.name) for item in search_in])
+    # TODO SET THIS BACK TO NORMAL
+    found_name = get_close_matches(normalize("NFKC", name), [normalize("NFKC", item.name) for item in search_in], n=3)
+    # found_item = find(lambda m: normalize("NFKC", m.name) == found_name[0], search_in)
+    found_item = [find(lambda m: normalize("NFKC", m.name) == found, search_in) for found in found_name]
     return found_item
