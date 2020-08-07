f5a88c375c7e0aef7d28280c9ea88c3459a5d0cd
Feven Kitsune <fevenkitsune@gmail.com>
2020-08-07 16:22:09 -0400

    + added the ability to find more details about a specific command

--- cogs/help.py
+++ cogs/help.py
@@ -21,9 +21,11 @@ class Help(commands.Cog):
     @commands.command(
         name="help",
         brief="Display this message.",
-        usage=""
+        usage="<command>",
+        help="The help command can be used to get a list of commands that are available to the user. "
+             "If you'd like to see more detailed information about a command, use `help <command>`."
     )
-    async def help(self, ctx):
+    async def help(self, ctx, *args):
         """Help menu. Processes the list of available commands into a readable menu."""
         em = discord.Embed(
             title="Fox Utilities Help Guide",
@@ -32,17 +34,26 @@ class Help(commands.Cog):
         )
         em.set_footer(text=generate_footer(ctx))
 
-        for cmd in sorted(self.client.commands, key=lambda command: command.cog_name):
-            # If not developer, do not show hidden commands.
-            if cmd.hidden and not (ctx.author.id == developer_id):
-                pass
-            else:
-                # Help field formatter.
+        # If asking about a specific command, search for that command or its aliases.
+        # Double := because I enjoy making Python programmers angry.
+        if args and (search := args[0]):
+            if command := self.client.get_command(search):
                 em.add_field(
-                    name=f"{'#' if cmd.hidden else ''}`{cmd.cog_name}`> {cmd.name} {cmd.usage}",
-                    value=cmd.brief,
-                    inline=False
+                    name=f"{'#' if command.hidden else ''}`{command.cog_name}`\n{command.name} {command.usage}",
+                    value=command.help
                 )
+        else:
+            for cmd in sorted(self.client.commands, key=lambda command: command.cog_name):
+                # If not developer, do not show hidden commands.
+                if cmd.hidden and not (ctx.author.id == developer_id):
+                    pass
+                else:
+                    # Help field formatter.
+                    em.add_field(
+                        name=f"{'#' if cmd.hidden else ''}`{cmd.cog_name}`> {cmd.name} {cmd.usage}",
+                        value=cmd.brief,
+                        inline=False
+                    )
 
         await ctx.author.send(embed=em)
 
