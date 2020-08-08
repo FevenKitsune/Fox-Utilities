b6db0b638c2f2d16c28ca729145e171df81bea7d
Feven Kitsune <fevenkitsune@gmail.com>
2020-08-07 23:18:03 -0400

    + closes #20

--- cogs/guildmail.py
+++ cogs/guildmail.py
@@ -4,7 +4,6 @@ Author: Feven Kitsune <fevenkitsune@gmail.com>
 This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License.
 """
 
-import discord
 from discord.ext.commands import guild_only, dm_only
 from utility.checks import *
 from utility.findbyname import find_by_name
