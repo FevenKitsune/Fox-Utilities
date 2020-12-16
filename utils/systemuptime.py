d894cd383d38d0421b35c482ccca4391ae5ae946
Feven Kitsune <fevenkitsune@gmail.com>
2020-12-16 17:34:39 -0500

    - removed dead file

--- utils/systemuptime.py
+++
@@ -1,6 +0,0 @@
-"""
-Fox Utilities > systemuptime.py
-Author: Feven Kitsune <fevenkitsune@gmail.com>
-This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License.
-"""
-
