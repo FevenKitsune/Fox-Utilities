8ba7c4a9721279ddb569f89c73d9c536dfa3652e
Will Stewart <fevenkitsune@gmail.com>
2022-02-18 18:28:08 -0500

    Fixed missing import

--- cogs/info/about.py
+++ cogs/info/about.py
@@ -2,7 +2,7 @@ import discord
 from discord import Embed
 from discord.ext.commands import Cog, slash_command
 
-from config.globals import message_color, bot_invite, bot_development_server, bot_source, bot_wiki
+from config.globals import message_color, bot_development_server, bot_source, bot_wiki
 from utils.generators import generate_footer
 
 
