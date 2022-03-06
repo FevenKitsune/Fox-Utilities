a899ecf66543f74bb5858eba1e80e8d1cf5d4680
Will Stewart <fevenkitsune@gmail.com>
2022-03-06 14:17:47 -0500

    Converted members to a slash command. Still a work in progress.

--- cogs/stats/members.py
+++ cogs/stats/members.py
@@ -1,7 +1,10 @@
+import discord
 from discord import Embed
-from discord.ext.commands import Cog, command, guild_only
+from discord.ext.commands import Cog, slash_command, guild_only
+from discord.ext import pages
+from discord.commands import Option
 
-from config.globals import message_color, bot_member_page_size
+from config.globals import message_color, bot_member_page_size, developer_guild_id
 from utils.chunklist import chunklist
 from utils.findbyname import find_by_name
 from utils.generators import generate_footer
@@ -14,65 +17,42 @@ class Members(Cog):
     def __init__(self, client):
         self.client = client
 
-    @command(
+    @slash_command(
         name="members",
-        aliases=["member", "memlist"],
-        brief="Lists all members in a mentioned role.",
-        usage="@role/\"role_name\" [page #]",
-        help="The members command can be used to retrieve a list of members within a given role.\n\n"
-             "**Usage Information**\n"
-             "*@role_tag/\"role_name\"*: The desired role can be targeted by either tagging the role, or typing the "
-             "name of the role in quotation marks. Roles using 𝓯𝓪𝓷𝓬𝔂 𝓽𝓮𝔁𝓽 will automatically be interpreted as "
-             "standard QWERTY letters.\n"
-             "*page #*: Member lists containing more than 25 members will be split between multiple pages. To see "
-             "more pages, specify which page number to view."
+        description="Lists all members in a mentioned role.",
+        guild_ids=[developer_guild_id]
     )
     @guild_only()
-    async def member_list(self, ctx, *args):
+    async def member_list(
+            self,
+            ctx: discord.ApplicationContext,
+            role: Option(discord.Role, description="Role to grab members from.")
+    ):
         """Post a formatted list of the members in a given role."""
-        if len(args) < 1:
-            raise UserWarning("You must mention or name one role for this command")
-
-        # If no mentions, do search.
-        if len(ctx.message.role_mentions) < 1:
-            found_role = await find_by_name(args[0], ctx.message.guild.roles)
-            # If no roles found, error.
-            if found_role is None:
-                raise UserWarning("You must mention or name one role for this command.")
-        else:
-            found_role = ctx.message.role_mentions[0]
-
-        # Parse page argument. Defaults to the 1st page.
-        page_count = 1 if (len(args) < 2) else int(args[1])
-
         # Generates a list containing n sized chunks of found_role.members
-        chunked_members = chunklist(found_role.members, bot_member_page_size)
+        chunked_members = chunklist(role.members, bot_member_page_size)
 
-        em = Embed(
-            title=f":memo: {make_renderable(found_role.name)} Member List",
-            color=message_color
-        )
-        em.set_footer(
-            text=f"Page {page_count}/{str(len(chunked_members))} | {generate_footer(ctx)}"
-        )
+        if not len(chunked_members):
+            raise UserWarning("This role has no members!")
 
-        try:
-            # Grabs the list of members in the given index and generates embed fields.
-            for member in chunked_members[page_count - 1]:
-                em.add_field(
-                    name=f":high_brightness: {make_renderable(member.top_role.name)}",
-                    value=f"`User` {member.mention}\n"
-                          f"`Tag` {member.name}#{member.discriminator}"
-                )
-        except IndexError:
-            # Find cause of IndexError
-            if page_count > len(chunked_members) != 0:
-                raise UserWarning("There are no more pages for this role!")
-            elif len(chunked_members) == 0:
-                raise UserWarning("This role has no members!")
-            else:
-                raise UserWarning("An unknown IndexError has occured!")
-        await ctx.send(embed=em)
+        member_pages = []
+        for index, member_chunk in enumerate(chunked_members):
+            page_generator = discord.Embed(
+                title=f":memo: {make_renderable(role.name)} Member List",
+                color=message_color
+            )
+            page_generator.set_footer(
+                text=f"{generate_footer(ctx)}"
+            )
+            for member in member_chunk:
+                page_generator.add_field(name=f"{match_emoji(member.status)} {member}",
+                                         value=f"`User` {member.mention}\n"
+                                               f"`Tag` {make_renderable(member.top_role.name)}"
+                                         )
+            member_pages.append(page_generator)
+
+        paginator = pages.Paginator(pages=member_pages)
+        await paginator.respond(ctx.interaction, ephemeral=False)
 
 
 def setup(client):
